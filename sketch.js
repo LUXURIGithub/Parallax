e2e1b24c6cee444367d095ddaafe9b8ca63f3648
LUXURIGithub <132486060+LUXURIGithub@users.noreply.github.com>
2023-08-03 20:00:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,48 @@
+//Dino load
 let bgImg
 let xBg = 0
-
+let dinoImages = []
 let bgCloud
 let xCloud = 0
+let currentDinoImage = 0
+
+
+//Gravity load
+let y; 
+let ySpeed;
+let gravity = 0.3; 
+let isOnGround = true; 
 
 function preload() {
   bgImg = loadImage('assets/bg.jpeg')
   bgCloud = loadImage('assets/cloud.png')
+  for (let i = 1; i <= 8; i++) {
+    dinoImages.push(loadImage(`assets/dino/${i}.png`))
+  }
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight)
+  y = 400;
+  ySpeed = 0;
 }
 
 function draw() {
   moveBgImg()
   moveCloud()
+  drawDino()
+  
+  ySpeed += gravity;
+  y += ySpeed;
+  
+  if (y > 400) {
+  y = 400;
+  ySpeed *= -0.5; // Fazer a bola "quicar" ao chegar no chão
+  isOnGround = true;
+  } else {
+  isOnGround = false; // A bola está no ar se não chegou ao chão
+  }
+  
 }
 
 function moveBgImg() {
@@ -34,8 +61,25 @@ function moveCloud() {
   image(bgCloud, xCloud, 150, 100, 100)
   image(bgCloud, xCloud + width, 150, 100, 100)
   
-  xCloud -= 1.4
+  xCloud -= 0.5
   if (xCloud <= - width) {
     xCloud = 0
   }
 }
+
+function drawDino() {
+  clear()
+  moveBgImg()
+  moveCloud()
+  image(dinoImages[currentDinoImage], 30, y, 150, 150)
+  if (frameCount % 5 === 0) {
+    currentDinoImage = (currentDinoImage + 1) % dinoImages.length;
+  }
+}
+
+
+function keyPressed() {
+  if (keyCode === 32 && isOnGround) {  
+    ySpeed = -10; 
+  }
+} 
